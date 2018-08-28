a186dd5a24a02c14249f9569034f8446cfa4099b
Marten Hoekstra <marten@dolhuis.nl>
2018-08-28 14:19:16 +0200

    Add Messagedriven Services
    In the GOB infrastructure many messadriven services will be present,
    this provides the generic code for connection to the message bus.

---
+++ gobcore/message_broker/messagedriven_service.py
@@ -0,0 +1,92 @@
+import time
+
+from gobcore.message_broker.async_message_broker import AsyncConnection
+from gobcore.message_broker.config import WORKFLOW_EXCHANGE, CONNECTION_PARAMS
+
+keep_running = True
+
+
+def messagedriven_service(service_defenition):
+    """Start a connection with a the message broker and the given definition
+
+    SERVICE_DEFINITION is a dict of dicts:
+
+    ```
+    servicedefenition = {
+        'message_to_listen_to': {
+            'queue': 'name_of_the_queue_to_listen_to',
+            'handler': 'method_to_invoke_on_message',
+            'report_back': 'key_of_the_return_message',
+            'report_queue': 'name_of_the_queue_to_report_to'
+        }
+    }
+    ```
+
+    start the service with:
+
+    ```
+    from gobcore.message_broker.messagedriven_service import messagedriven_service
+
+    messagedriven_services(servicedefenition)
+
+    """
+
+    with AsyncConnection(CONNECTION_PARAMS) as connection:
+        # Subscribe to the queues, handle messages in the on_message function (runs in another thread)
+        for key, service in service_defenition.items():
+            queue = {
+                "exchange": WORKFLOW_EXCHANGE,
+                "name": service['queue'],
+                "key": key
+            }
+
+            on_message = _get_on_message(service)
+
+            connection.subscribe([queue], on_message)
+            print(f"Listening to messages {key} on {service['queue']}")
+
+        # Repeat forever
+        print("Queue connection for servicedefinition started")
+        while keep_running:
+            time.sleep(60)
+            # Report some statistics or whatever is useful
+            print(".")
+
+
+def _get_on_message(single_service):
+    """Prepare the generic on_message method with single_service specific handler, report_back_queue and
+    report back message"""
+
+    def on_message(connection, queue, key, msg):
+        """Called on every message receipt
+
+        :param connection: the connection with the message broker
+        :param queue: the message broker queue
+        :param key: the identification of the message (e.g. fullimport.proposal)
+        :param msg: the contents of the message
+        :return:
+        """
+
+        print(f"{key} accepted from {queue['name']}, start handling")
+
+        # Get the handler for the specific workflow
+        handle = single_service['handler']
+
+        try:
+            result_msg = handle(msg)
+        except RuntimeError:
+            return False
+
+        # Get the key to report back the results
+        report_back = single_service['report_back']
+
+        report_queque = {
+            "exchange": WORKFLOW_EXCHANGE,
+            "name": single_service['report_queue'],
+            "key": single_service['report_back']
+        }
+
+        connection.publish(report_queque, report_back, result_msg)
+        return True
+
+    return on_message
---
+++ tests/gobcore/test_message_service.py
@@ -0,0 +1,114 @@
+import random
+import string
+
+import mock
+import unittest
+
+
+from gobcore.message_broker.async_message_broker import AsyncConnection
+from gobcore.message_broker.config import WORKFLOW_EXCHANGE, CONNECTION_PARAMS
+from gobcore.message_broker import messagedriven_service
+
+
+def random_string(length=12, source=None):
+
+    if source is None:
+        source = string.ascii_letters
+
+    return ''.join(random.choice(source) for x in range(length))
+
+
+def get_service_fixture(handler):
+    return {
+        random_string(): {
+            'queue': random_string(),
+            'handler': handler,
+            'report_back': random_string(),
+            'report_queue': random_string()
+        }
+    }
+
+
+def handler(msg):
+
+    global return_message
+    return return_message
+
+
+def mock_get_on_message(service):
+
+    global return_method
+    return return_method
+
+
+class AnyOf:
+    def __init__(self, list):
+        self.list = list
+
+    def __eq__(self, other):
+        for item in list:
+            if other.__eq__(item):
+                return True
+        return False
+
+
+class TestMessagedrivenService(unittest.TestCase):
+
+    def test_get_on_message(self):
+
+        global return_message
+
+        # setup mocks and fixtures
+        mocked_handler = mock.Mock(wraps=handler)
+        service = get_service_fixture(mocked_handler)
+        key = [k for k in service.keys()][0]
+        single_service = [v for v in service.values()][0]
+
+        message = random_string()
+        queue = {'name': single_service['queue']}
+        connection = AsyncConnection({})
+
+        # setup expectations
+        return_message = random_string()
+        return_key = single_service['report_back']
+        return_queue = {
+            "exchange": WORKFLOW_EXCHANGE,
+            "name": single_service['report_queue'],
+            "key": return_key
+        }
+
+        with mock.patch.object(connection, "publish") as mocked_publish:
+
+            on_message = messagedriven_service._get_on_message(single_service)
+            result = on_message(connection, queue, key, message)
+
+            # The result should be True
+            self.assertTrue(result)
+
+            # The message handler should be called with the message
+            mocked_handler.assert_called_with(message)
+
+            # The return message should be published on the return queue
+            mocked_publish.assert_called_with(return_queue, return_key, return_message)
+
+    @mock.patch("gobcore.message_broker.messagedriven_service.AsyncConnection")
+    @mock.patch("gobcore.message_broker.messagedriven_service._get_on_message",
+                side_effect=mock_get_on_message)
+    def test_messagedriven_service(self, get_on_message, mocked_connection):
+
+        global return_method
+        return_method = random_string()
+
+        service_definition = get_service_fixture(handler)
+        expected_key = [k for k in service_definition.keys()][0]
+        single_service = [v for v in service_definition.values()][0]
+        expected_queue = single_service['queue']
+
+        messagedriven_service.keep_running = False
+        messagedriven_service.messagedriven_service(service_definition)
+
+        mocked_connection.assert_called_with(CONNECTION_PARAMS)
+        mocked_connection.return_value.__enter__.return_value.subscribe\
+            .assert_called_with([{'exchange': WORKFLOW_EXCHANGE,
+                                  'name': expected_queue,
+                                  'key': expected_key}], return_method)

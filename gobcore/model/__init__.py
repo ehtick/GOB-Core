beb122a5d2201c1b2650e08ded8fff3b3bfd5859
Jasper Jager <mail@jasperjager.com>
2018-10-01 13:48:27 +0200

    Change GOBModel to reflect documentation
    GOBModel for meetbout was changed to match the current specification. The function get_model_fields was added to be used for database creation. It adds the _text and _id fields for references in the model.

--- gobcore/model/__init__.py
+++ gobcore/model/__init__.py
@@ -9,8 +9,33 @@ class GOBModel():
             data = json.load(file)
         self._data = data
 
+        # Add fields to the GOBModel to be used in database creation and lookups
+        self._add_fields()
+
+    def _add_fields(self):
+        for entity_name, model in self._data.items():
+            self._data[entity_name]['fields'] = self._expand_references(model['attributes'])
+
+    def _expand_references(self, attributes):
+        fields = {}
+        for field_name, attributes in attributes.items():
+            if 'ref' in attributes:
+                ref = attributes.pop('ref', None)
+                # For references add the _text and _id fields with type GOB.String
+                fields[f'{field_name}_text'] = attributes
+                fields[f'{field_name}_id'] = {
+                    'type': "GOB.String",
+                    'description': f"Reference ID to {ref}"
+                }
+            else:
+                fields[field_name] = attributes
+        return fields
+
     def get_model(self, name):
         return self._data[name]
+
+    def get_model_fields(self, name):
+        return self._data[name]['fields']
 
     def get_model_names(self):
         return [k for k, v in self._data.items()]

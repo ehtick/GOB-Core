1000f50b0f68ccdb22ce85c7025dc9093142d424
vegter <amsjurien@gmail.com>
2018-10-04 10:33:31 +0200

    Define RabbitMQ vhost and port

--- gobcore/message_broker/config.py
+++ gobcore/message_broker/config.py
@@ -12,12 +12,14 @@ import os
 import pika
 
 MESSAGE_BROKER = os.getenv("MESSAGE_BROKER_ADDRESS", "localhost")
+MESSAGE_BROKER_PORT = os.getenv("MESSAGE_BROKER_PORT", 15672)
+MESSAGE_BROKER_VHOST = os.getenv("MESSAGE_BROKER_VHOST", "gob")
 MESSAGE_BROKER_USER = os.getenv("MESSAGE_BROKER_USERNAME", "guest")
 MESSAGE_BROKER_PASSWORD = os.getenv("MESSAGE_BROKER_PASSWORD", "guest")
 
 CONNECTION_PARAMS = pika.ConnectionParameters(
     host=MESSAGE_BROKER,
-    virtual_host="gob",
+    virtual_host=MESSAGE_BROKER_VHOST,
     credentials=pika.PlainCredentials(username=MESSAGE_BROKER_USER,
                                       password=MESSAGE_BROKER_PASSWORD)
 )

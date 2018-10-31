92fa4104f28e098b4029e82c3543fdbf899a4907
Jasper Jager <mail@jasperjager.com>
2018-10-31 16:19:36 +0100

    Add references to model definition
    An extra attribute references was added for easy access. The model json was updated to have catalog:collection in the ref field

--- gobcore/model/__init__.py
+++ gobcore/model/__init__.py
@@ -9,8 +9,15 @@ class GOBModel():
             data = json.load(file)
         self._data = data
 
+        # Extract references for easy access in API
+        for entity_name, model in self._data.items():
+            self._data[entity_name]['references'] = self._extract_references(model['attributes'])
+
         # Add fields to the GOBModel to be used in database creation and lookups
         self._add_fields()
+
+    def _extract_references(self, attributes):
+        return {field_name: spec for field_name, spec in attributes.items() if 'ref' in spec}
 
     def _add_fields(self):
         for entity_name, model in self._data.items():
@@ -20,12 +27,11 @@ class GOBModel():
         fields = {}
         for field_name, attributes in attributes.items():
             if 'ref' in attributes:
-                ref = attributes.pop('ref', None)
-                # For references add the _text and _id fields with type GOB.String
+                # For references add the _text and _id fields
                 fields[f'{field_name}_text'] = attributes
                 fields[f'{field_name}_id'] = {
                     'type': attributes['type'],
-                    'description': f"Reference ID to {ref}"
+                    'description': f"Reference ID to {attributes['ref']}"
                 }
             else:
                 fields[field_name] = attributes

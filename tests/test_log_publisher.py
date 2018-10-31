a41411aa4b8c6c2f1795db739cd8f1e1e9105e53
vegter <amsjurien@gmail.com>
2018-10-31 10:28:07 +0100

    Auto (re-)connect to log queue

--- tests/test_log_publisher.py
+++ tests/test_log_publisher.py
@@ -12,22 +12,26 @@ class TestLogPublisher(unittest.TestCase):
         assert(publisher is not None)
 
 
+    @mock.patch('gobcore.message_broker.message_broker.Connection.is_open', return_value=True)
     @mock.patch('gobcore.message_broker.message_broker.Connection.publish')
-    def testPublish(self, patched_publish):
+    def testPublish(self, patched_publish, pathed_is_open):
         publisher = LogPublisher(None)
         publisher.publish("Level", "Message")
         assert(patched_publish.called)
+        assert(pathed_is_open.called)
 
 
+    @mock.patch('gobcore.message_broker.message_broker.Connection.is_open', return_value=True)
     @mock.patch('gobcore.message_broker.message_broker.Connection.connect')
-    def testConnect(self, patched_connect):
+    def testConnect(self, patched_connect, pathed_is_open):
         publisher = LogPublisher(None)
         publisher.connect()
         assert(patched_connect.called)
 
 
+    @mock.patch('gobcore.message_broker.message_broker.Connection.is_open', return_value=True)
     @mock.patch('gobcore.message_broker.message_broker.Connection.disconnect')
-    def testDisconnect(self, patched_disconnnect):
+    def testDisconnect(self, patched_disconnnect, pathed_is_open):
         publisher = LogPublisher(None)
         publisher.disconnect()
         assert(patched_disconnnect.called)

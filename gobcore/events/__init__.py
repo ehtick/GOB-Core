7de6804368709183ead5aba1873cc14b1fc555b7
vegter <amsjurien@gmail.com>
2018-12-10 12:46:29 +0100

    Rewrote get event code
    The code has been reorganised to be more maintainable
    Also the version now supports changes in entity sources

--- gobcore/events/__init__.py
+++ gobcore/events/__init__.py
@@ -38,35 +38,53 @@ def _get_event(name):
         raise GOBException(f"{name} is an invalid GOB event")
 
 
-def get_event_for(entity, data, metadata, modifications):
+def get_event_for(old_data, new_data, modifications):
     """
-    Get the event dict for the given entity, source-data, metadata and calculated modifications
+    Get the event for the given entity, source-data, metadata and calculated modifications
 
-    :param entity:
-    :param data:
-    :param metadata:
-    :param modifications:
+    :param old_data: the entity data as stored in the database
+    :param new_data: the entity data as received in the message
+    :param modifications: the list of modifications for the entity (old_data - new_data)
 
-    :return: the event dict
+    :return: the event that corresponds with the given parameters
     """
-    has_old_state = entity is not None
-    has_new_state = data is not None
+    has_old_state = old_data is not None
+    has_new_state = new_data is not None
+    assert has_old_state or has_new_state, "Event without old or new state"
+
     has_modifications = len(modifications) > 0
 
+    # Get the event class (ADD, MODIFY, CONFIRM, DELETE)
     gob_event = _get_event_class_for(has_old_state, has_new_state, has_modifications)
 
-    # get relevenant id's from either entity or data
-    _source_id = data["_source_id"] if has_new_state else None
     if has_old_state:
-        _entity_source_id = entity._source_id
-        last_event = entity._last_event
+        # MODIFY, CONFIRM, DELETE
+        # Attach event to the previous event for the entity
+        _last_event = old_data._last_event
+        # The source_id might change, so save the old entity source_id
+        _entity_source_id = old_data._source_id
     else:
-        _entity_source_id = _source_id
-        last_event = None
+        # ADD
+        # No previous event, entity source id is equal to the new source id
+        _last_event = None
+        _entity_source_id = new_data["_source_id"]
 
-    modifications_dict = dict(modifications=modifications)
-    data = modifications_dict if data is None else {**modifications_dict, **data}
-    data = {**data, "_last_event": last_event}
+    if has_new_state:
+        # ADD, MODIFY, CONFIRM
+        # Register the source of the event
+        _source_id = new_data["_source_id"]
+    else:
+        # DELETE
+        # No new source id, keep existing source id
+        _source_id = old_data._source_id
+
+    # The event data are the modifications
+    data = dict(modifications=modifications)
+    data["_last_event"] = _last_event
+    if has_new_state:
+        # Merge the new state data (possible ADD event)
+        # The ultimate event class will filter either the modifications (MODIFY) or the new data (ADD)
+        data.update(new_data)
 
     return gob_event.create_event(_source_id, _entity_source_id, data)
 

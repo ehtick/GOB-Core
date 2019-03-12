6debf50df6bcaf239edc9d2ee68de91cde503c34
vegter <amsjurien@gmail.com>
2019-03-12 17:06:08 +0100

    DateTime and Date JSON conversion
    Small change to improve compatibility

--- gobcore/typesystem/json.py
+++ gobcore/typesystem/json.py
@@ -2,7 +2,7 @@ import datetime
 import decimal
 import json
 
-from gobcore.typesystem.gob_types import GOBType, Date, DateTime
+from gobcore.typesystem.gob_types import GOBType
 
 
 class GobTypeJSONEncoder(json.JSONEncoder):
@@ -26,15 +26,13 @@ class GobTypeJSONEncoder(json.JSONEncoder):
             return json.loads(str(obj))
 
         if type(obj) is datetime.date:
-            # First convert datetime to string and use GOBType to create JSON output
-            string_value = datetime.datetime.strftime(obj, Date.internal_format)
-            obj = Date.from_value(string_value, **{'format': Date.internal_format})
-            return json.loads(obj.json)
+            return obj.isoformat()
 
         if type(obj) is datetime.datetime:
-            # First convert datetime to string and use GOBType to create JSON output
-            string_value = datetime.datetime.strftime(obj, DateTime.internal_format)
-            obj = DateTime.from_value(string_value, **{'format': DateTime.internal_format})
-            return json.loads(obj.json)
+            value = obj.isoformat()
+            if len(value) == len('YYYY-MM-DDTHH:MM:SS'):
+                # Add missing microseconds
+                value += '.000000'
+            return value
 
         return super().default(obj)

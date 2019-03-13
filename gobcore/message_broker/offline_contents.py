ee63024ed734563f873156a5a01cd88f21d20144
vegter <amsjurien@gmail.com>
2019-03-13 12:42:45 +0100

    Improve offload message contents
    Use gettotalsizeof to decide if a message should be offloaded
    
    Print error message when writing or reading offloaded contents
    
    On end message, clear contents and run garbage collection explicitly

--- gobcore/message_broker/offline_contents.py
+++ gobcore/message_broker/offline_contents.py
@@ -4,7 +4,7 @@ Large messages are stored outside the message broker.
 This prevents the message broker from transferring large messages
 
 """
-import sys
+import gc
 import uuid
 
 import os
@@ -13,9 +13,11 @@ from pathlib import Path
 
 from gobcore.message_broker.config import GOB_SHARED_DIR
 
-_MAX_CONTENTS_SIZE = 0   # Any message contents larger than this size is stored offline
-_CONTENTS = "contents"      # The name of the message attribute to check for its contents
-_CONTENTS_REF = "contents_ref"  # The name of the message attribute to store the reference to the offloaded contents
+from gobcore.utils import gettotalsizeof
+
+_MAX_CONTENTS_SIZE = 4096                   # Any message contents larger than this size is stored offline
+_CONTENTS = "contents"                      # The name of the message attribute to check for its contents
+_CONTENTS_REF = "contents_ref"              # The name of the message attribute to store the reference to the offloaded contents
 _MESSAGE_BROKER_FOLDER = "message_broker"   # The name of the folder where the offloaded contents are stored
 
 
@@ -53,16 +55,16 @@ def offload_message(msg, converter):
     """
     if _CONTENTS in msg:
         contents = msg[_CONTENTS]
-        size = sys.getsizeof(contents)
+        size = gettotalsizeof(contents)
         if size > _MAX_CONTENTS_SIZE:
             unique_name = _get_unique_name()
             filename = _get_filename(unique_name)
             try:
                 with open(filename, 'w') as file:
                     file.write(converter(contents))
-            except IOError:
+            except IOError as e:
                 # When the write fails, returns the msg untouched
-                print("Offload failed", filename)
+                print(f"Offload failed ({str(e)})", filename)
                 return msg
             # Replace the contents by a reference
             msg[_CONTENTS_REF] = unique_name
@@ -87,7 +89,7 @@ def load_message(msg, converter):
     return msg, unique_name
 
 
-def end_message(unique_name):
+def end_message(msg, unique_name):
     """Remove the offloaded contents after a message has been succesfully handled
 
     end_message can always be called, it can never fail
@@ -99,5 +101,10 @@ def end_message(unique_name):
         try:
             filename = _get_filename(unique_name)
             os.remove(filename)
-        except Exception:
-            pass
+        except Exception as e:
+            print(f"Remove failed ({str(e)})", filename)
+
+    # Clear message and run garbage collection
+    for item in msg.keys():
+        msg[item] = None
+    gc.collect()

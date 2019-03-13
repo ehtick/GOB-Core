80a419a884cbe48f78a005c745c582de2a26ebbd
vegter <amsjurien@gmail.com>
2019-03-13 12:29:14 +0100

    total size calculation
    sys.getsizeof only return the "bare" size of an object
    gettotalsizeof returns the full size of an object

---
+++ gobcore/utils.py
@@ -0,0 +1,34 @@
+from sys import getsizeof
+from itertools import chain
+
+def gettotalsizeof(o):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, dict, set and frozenset.
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {
+        tuple: iter,
+        list: iter,
+        dict: dict_handler,
+        set: iter,
+        frozenset: iter,
+    }
+    seen = set()  # track which object id's have already been seen
+    default_size = getsizeof(0)  # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:  # do not double count the same object
+            return 0
+
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)

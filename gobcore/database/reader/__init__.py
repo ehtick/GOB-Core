3979fb59012889b8c6a155557a7dfe53cd261eed
vegter <amsjurien@gmail.com>
2019-04-02 19:17:08 +0200

    Entity conversion per record

--- gobcore/database/reader/__init__.py
+++ gobcore/database/reader/__init__.py
@@ -1,6 +1,7 @@
-from gobcore.database.reader.database import read_from_database
-from gobcore.database.reader.oracle import read_from_oracle
-from gobcore.database.reader.objectstore import read_from_objectstore
-from gobcore.database.reader.file import read_from_file
+from gobcore.database.reader.database import query_database, read_from_database
+from gobcore.database.reader.oracle import query_oracle, read_from_oracle
+from gobcore.database.reader.objectstore import query_objectstore, read_from_objectstore
+from gobcore.database.reader.file import query_file, read_from_file
 
-__all__ = ['read_from_database', 'read_from_objectstore', 'read_from_file', 'read_from_oracle']
+__all__ = ['read_from_database', 'read_from_objectstore', 'read_from_file', 'read_from_oracle'] + \
+          ['query_database', 'query_objectstore', 'query_file', 'query_oracle']

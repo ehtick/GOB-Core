e6e03201d9122857cdb402e25790e8f8ea43caf6
Jaap Koetsier <jaap@chariot-it.nl>
2019-04-10 10:48:26 +0200

    Add query_postgresql

---
+++ gobcore/database/reader/postgresql.py
@@ -0,0 +1,22 @@
+from gobcore.exceptions import GOBException
+from psycopg2 import Error
+from psycopg2.extras import DictCursor
+
+
+def query_postgresql(connection, query: str):
+    """Query Postgres
+
+    :param connection:
+    :param query:
+    :return:
+    """
+    try:
+        cursor = connection.cursor(cursor_factory=DictCursor)
+        cursor.execute(query)
+        connection.commit()
+
+        for row in cursor:
+            yield row
+        cursor.close()
+    except Error as e:
+        raise GOBException(f'Error executing query: {query}. Error: {e}')

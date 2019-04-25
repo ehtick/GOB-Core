7d2221221f4b78d4d3757a8b196c03083cb2835a
vegter <amsjurien@gmail.com>
2019-04-25 08:39:05 +0200

    Add RESULT queue
    To report results of workflow steps
    The result is received by the workflow
    which takes care of any further routing

--- gobcore/message_broker/config.py
+++ gobcore/message_broker/config.py
@@ -32,14 +32,13 @@ CONNECTION_PARAMS = pika.ConnectionParameters(
 WORKFLOW_EXCHANGE = "gob.workflow"
 LOG_EXCHANGE = "gob.log"
 STATUS_EXCHANGE = "gob.status"
-RESULT_EXCHANGE = "gob.result"
 
 PROPOSAL_QUEUE = WORKFLOW_EXCHANGE + '.proposal'
 REQUEST_QUEUE = WORKFLOW_EXCHANGE + '.request'
 PREPARE_QUEUE = WORKFLOW_EXCHANGE + '.prepare'
 IMPORT_QUEUE = WORKFLOW_EXCHANGE + '.import'
 EXPORT_QUEUE = WORKFLOW_EXCHANGE + '.export'
-RESULT_QUEUE = RESULT_EXCHANGE + '.completed'
+RESULT_QUEUE = WORKFLOW_EXCHANGE + '.result'
 HEARTBEAT_QUEUE = STATUS_EXCHANGE + '.heartbeat'
 LOG_QUEUE = LOG_EXCHANGE + '.all'
 
@@ -70,9 +69,9 @@ QUEUES = [
         "key": "export.start"
     },
     {
-        "exchange": RESULT_EXCHANGE,
+        "exchange": WORKFLOW_EXCHANGE,
         "name": RESULT_QUEUE,
-        "key": "*.completed"
+        "key": "*.result"
     },
     {
         "exchange": LOG_EXCHANGE,

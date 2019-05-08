0c37a52dbc5b541142566f819b27a68a50d9f22e
Jasper Jager <mail@jasperjager.com>
2019-05-08 10:24:15 +0200

    Add indexes to events table

--- gobcore/model/sa/gob.py
+++ gobcore/model/sa/gob.py
@@ -170,6 +170,14 @@ def _relation_indexes_for_collection(catalog_name, collection_name, collection):
 def _derive_indexes() -> dict:
     indexes = {}
 
+    # Add indexes to events table
+    event_indexes = ['eventid', 'catalogue', 'entity', 'action', 'source', 'application']
+    for column in event_indexes:
+        indexes[f'events.idx.{column}'] = {
+            "columns": [column],
+            "table_name": "events"
+        }
+
     for catalog_name, catalog in model.get_catalogs().items():
         for collection_name, collection in model.get_collections(catalog_name).items():
             if collection['version'] != "0.1":

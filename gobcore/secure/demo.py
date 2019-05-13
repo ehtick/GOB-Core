a1cce4e8a3f047ae9b972a650d0b837c49e170bb
vegter <amsjurien@gmail.com>
2019-05-13 14:03:59 +0200

    Fix small bug in demo encrypt for None values

--- gobcore/secure/demo.py
+++ gobcore/secure/demo.py
@@ -7,9 +7,18 @@ Uses base64 encode/decode for demo purposes
 import base64
 
 
+def _encrypt(value, _):
+    return base64.b64encode(str(value).encode()).decode('UTF-8')
+
+
+def _decrypt(value, _):
+    value = base64.b64decode(value).decode('UTF-8')
+    return None if value == "None" else value
+
+
 demo = {
     "index": 0,
     "secret": "DEMO",
-    "encrypt": lambda value, secret: base64.b64encode(str(value).encode()).decode('UTF-8'),
-    "decrypt": lambda value, secret: base64.b64decode(value).decode('UTF-8')
+    "encrypt": _encrypt,
+    "decrypt": _decrypt
 }

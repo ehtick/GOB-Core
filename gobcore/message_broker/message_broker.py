917264db9877353ab084b8916ba1076f914510d0
vegter <amsjurien@gmail.com>
2019-10-07 09:57:31 +0200

    Remove airflow specific code from GOB Core
    This code is aurflow specific and not part of GOB-Core

--- gobcore/message_broker/message_broker.py
+++ gobcore/message_broker/message_broker.py
@@ -74,64 +74,6 @@ class Connection(object):
             body=json_msg
         )
 
-    def consume(self, exchange, queue, key, message_matched, ack=False, inactivity_timeout=10):
-        """Blocking consumption of one single message from a queue without using a callback.
-
-        Pika `BlockingChannel.consume()` method is a generator that yields
-        each message as a tuple of method, properties, and body.
-        The active generator iterator terminates when the
-        consumer is cancelled by client via `BlockingChannel.cancel()` or by
-        broker.
-
-        Pika advices us to call `BlockingChannel.cancel()` when you escape out of the
-        generator loop.
-
-        If you don't do this, then next call on the same channel to `BlockingChannel.consume()`
-        with the exact same (queue, no_ack, exclusive) parameters
-        will resume the existing consumer generator; however, calling with
-        different parameters will result in an exception.
-
-        :param str exchange: The exchange to use as a search criteria.
-        :param str queue: The queue name to consume.
-        :param str key: Routing key to use as a search criteria.
-        :param callable message_matched: A function with only one parameter msg.
-            It should return True or False based on if the msg matches our search criteria.
-            That gives us necessary flexibity with defining how we look for messages.
-            This also makes this method independent from a business logic
-            which can be defined in message_matched function (provided by the consumer).
-        :param bool ack: Ack or not ack the consumed message. Default False.
-        :param int inactivity_timeout: Inactivity timeout in seconds. Default 10.
-            We might want to re-evaluate if this value is suitable for us.
-            - If inactivity_timeout is defined (not None), it will cause
-            pika `BlockingChannel.consume()` method to yield (None, None, None)
-            after the given period of inactivity.
-            - If inactivity_timeout is None, then pika consume method blocks
-            until the next event arrives. We default inactivity_timeout to
-            an arbitrary value to avoid this situation.
-        """
-
-        # pika consume yields: tuple(spec.Basic.Deliver, spec.BasicProperties, body as str or unicode)
-        for method, properties, body in self._channel.consume(queue, inactivity_timeout=inactivity_timeout):
-            # break if no more messages can be consumed after
-            # a certain period of time (defined by inactivity_timeout)
-            if not method:
-                break
-
-            if method.exchange == exchange and method.routing_key == key:
-                # extract msg and offload_id from the body so we can analyse it
-                msg, offload_id = get_message_from_body(body, self._params)
-                # check if msg matches with our search criteria
-                if message_matched(msg):
-                    # optionally ack the message
-                    if ack:
-                        self._channel.basic_ack(method.delivery_tag)
-                    return method, properties, msg, offload_id
-
-        # we call cancel() to escape out of the generator loop
-        self._channel.cancel()
-        # No messages found or all available messages do not match our search criteria
-        return (None, None, None, None)
-
     def disconnect(self):
         """Disconnect from RabbitMQ
 

6688965ac81050831bfef5f222d3c33b1912e4a8
Jaap Koetsier <jaap@chariot-it.nl>
2019-10-24 17:07:59 +0200

    Don't use fancy indexes for compound indexes

--- tests/gobcore/model/sa/test_gob.py
+++ tests/gobcore/model/sa/test_gob.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from gobcore.model.sa.gob import models, model, _derive_models, _derive_indexes, GOBModel
+from gobcore.model.sa.gob import models, model, _derive_models, _derive_indexes, GOBModel, _get_special_column_type
 
 
 class TestGob(unittest.TestCase):
@@ -13,3 +13,10 @@ class TestGob(unittest.TestCase):
         for (name, cls) in models.items():
             m = cls()
             self.assertEqual(str(m), name)
+
+    @patch("gobcore.model.sa.gob.is_gob_geo_type", lambda x: x is not None and x.startswith('geo'))
+    @patch("gobcore.model.sa.gob.is_gob_json_type", lambda x: x is not None and x.startswith('json'))
+    def test_get_special_column_type(self):
+        self.assertEqual("geo", _get_special_column_type("geocolumn"))
+        self.assertEqual("json", _get_special_column_type("jsoncolumn"))
+        self.assertIsNone(_get_special_column_type(None))

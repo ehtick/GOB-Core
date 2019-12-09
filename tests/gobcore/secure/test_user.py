05899fbc450c16713a399496bbef858e091d7848
vegter <amsjurien@gmail.com>
2019-12-09 11:14:04 +0100

    Link to keycloak
    Define headers and roles
    Only gib_adm has access to secure values (for now)

--- tests/gobcore/secure/test_user.py
+++ tests/gobcore/secure/test_user.py
@@ -1,30 +1,33 @@
-import unittest
-import json
+from unittest import TestCase, mock
 
-from gobcore.secure.config import LEVELS, ROLES
+from gobcore.secure.config import REQUEST_ROLES, GOB_ADMIN
 from gobcore.secure.user import User
 
 
-class TestUser(unittest.TestCase):
+class TestUser(TestCase):
 
     def setup(self):
         pass
 
-
     def test_create(self):
-        user = User(None)
+        mock_request = mock.MagicMock()
+        mock_request.headers = {
+            REQUEST_ROLES: GOB_ADMIN
+        }
+        user = User(mock_request)
         self.assertIsNotNone(user._roles)
 
     def test_has_access(self):
-        user = User(None)
+        mock_request = mock.MagicMock()
+        mock_request.headers = {}
+        user = User(mock_request)
+        value = "any encrypted value"
+
         user._roles = []
-        value = json.dumps({"l": ROLES["user"]})
         self.assertFalse(user.has_access_to(value))
 
-        user._roles = ["user"]
-        value = json.dumps({"l": ROLES["user"]})
-        self.assertTrue(user.has_access_to(value))
+        user._roles = ["any user"]
+        self.assertFalse(user.has_access_to(value))
 
-        user._roles = ["user"]
-        value = json.dumps({"l": ROLES["admin"]})
+        user._roles = [GOB_ADMIN]
         self.assertTrue(user.has_access_to(value))

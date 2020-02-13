ef6081c6d0087a8e71020e0e4571b78093893883
vegter <amsjurien@gmail.com>
2020-02-13 09:19:43 +0100

    NameCompressor for compress/uncompress tabel names

---
+++ gobcore/model/name_compressor.py
@@ -0,0 +1,26 @@
+class NameCompressor():
+    _CONVERSIONS = {
+        "aantekening": "ant",
+        "kadastraal": "kad",
+        "appartements": "app",
+        "splitsing": "spl",
+        "ontstaan_uit": "ont",
+        "is_bron_voor": "brn",
+        "betrokken_bij": "btr"
+    }
+
+    @classmethod
+    def _compressed_value(cls, value):
+        return f"_{value}_"
+
+    @classmethod
+    def compress_name(cls, name):
+        for src, dst in cls._CONVERSIONS.items():
+            name = name.replace(src, cls._compressed_value(dst))
+        return name
+
+    @classmethod
+    def uncompress_name(cls, name):
+        for src, dst in cls._CONVERSIONS.items():
+            name = name.replace(cls._compressed_value(dst), src)
+        return name
---
+++ tests/gobcore/model/test_name_compressor.py
@@ -0,0 +1,27 @@
+import unittest
+
+from gobcore.model.name_compressor import NameCompressor
+
+
+class TestNameCompressor(unittest.TestCase):
+
+    def test_conversions(self):
+        names = [
+            "",
+            "is_bron_voor_aantekening_kadastraal_object",
+            "betrokken_bij_appartementsrechtsplitsing_vve",
+            "ontstaan_uit_appartementsrechtsplitsing_vve",
+            "some string"
+        ]
+
+        # Don't have conversions without any profit
+        for key, value in NameCompressor._CONVERSIONS.items():
+            conversion = NameCompressor._compressed_value(value)
+            self.assertTrue(len(conversion) < len(key))
+
+        # uncompress(compress(name)) == name
+        # conversions can never result in longer strings than the originale
+        for name in names:
+            conversion = NameCompressor.compress_name(name)
+            self.assertTrue(len(conversion) <= len(name))
+            self.assertEqual(NameCompressor.uncompress_name(conversion), name)

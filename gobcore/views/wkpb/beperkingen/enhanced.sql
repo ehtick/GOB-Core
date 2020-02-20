60b88c05ed592a64e450aa8929101eec629268ab
Onno <onno.logchies@xifeo.nl>
2020-02-20 11:16:56 +0100

    fix _date_deleted

--- gobcore/views/wkpb/beperkingen/enhanced.sql
+++ gobcore/views/wkpb/beperkingen/enhanced.sql
@@ -11,8 +11,7 @@ FROM
     wkpb_beperkingen bpg 
 WHERE "status" -> 'code' = '3' 
       AND aard -> 'code' <> '3'
-      AND (bpg._expiration_date IS NULL 
-        OR bpg._expiration_date > NOW()) 
       AND COALESCE(_expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
+      AND _date_deleted IS NULL
 ORDER BY 
     bpg.identificatie
--- gobcore/views/wkpb/brondocumenten/enhanced.sql
+++ gobcore/views/wkpb/brondocumenten/enhanced.sql
@@ -8,8 +8,7 @@ FROM
     wkpb_beperkingen bpg
 WHERE bpg.status -> 'code' = '3'
       AND bpg.aard -> 'code' <> '3'
-      AND (bpg._expiration_date IS NULL 
-        OR bpg._expiration_date > now())
       AND COALESCE(_expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
+      AND _date_deleted IS NULL
 ORDER BY 
     bpg.identificatie

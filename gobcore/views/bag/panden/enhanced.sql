b099504a5e74788ea6a54af7cf3d71e7c147fad6
Jasper Jager <mail@jasperjager.com>
2020-03-12 16:39:30 +0100

    Use relation tables on PND enhanced views

--- gobcore/views/bag/panden/enhanced.sql
+++ gobcore/views/bag/panden/enhanced.sql
@@ -15,73 +15,67 @@
       pnd.laagste_bouwlaag,
       pnd.type_woonobject,
       json_build_object(
-        'identificatie', ligt_in_bouwblok.identificatie,
-        'code', ligt_in_bouwblok.code
+        'identificatie', bbk_0.identificatie,
+        'code', bbk_0.code
       ) AS _ref_ligt_in_bouwblok_gbd_bbk,
       json_build_object(
-        'identificatie', ligt_in_buurt.identificatie,
-        'code', ligt_in_buurt.code,
-        'naam', ligt_in_buurt.naam
+        'identificatie', brt_0.identificatie,
+        'code', brt_0.code,
+        'naam', brt_0.naam
       ) AS _ref_ligt_in_buurt_gbd_brt,
       json_build_object(
-        'identificatie', ligt_in_wijk.identificatie,
-        'code', ligt_in_wijk.code,
-        'naam', ligt_in_wijk.naam
+        'identificatie', wijk_0.identificatie,
+        'code', wijk_0.code,
+        'naam', wijk_0.naam
       ) AS _ref_ligt_in_wijk_gbd_wijk,
       json_build_object(
-        'identificatie', ligt_in_ggwgebied.identificatie,
-        'code', ligt_in_ggwgebied.code,
-        'naam', ligt_in_ggwgebied.naam
+        'identificatie', ggw_0.identificatie,
+        'code', ggw_0.code,
+        'naam', ggw_0.naam
       ) AS _ref_ligt_in_ggwgebied_gbd_ggw,
       json_build_object(
-        'identificatie', ligt_in_stadsdeel.identificatie,
-        'code', ligt_in_stadsdeel.code,
-        'naam', ligt_in_stadsdeel.naam
+        'identificatie', sdl_0.identificatie,
+        'code', sdl_0.code,
+        'naam', sdl_0.naam
       ) AS _ref_ligt_in_stadsdeel_gbd_sdl,
       json_build_object(
-        'identificatie', ligt_in_ggpgebied.identificatie,
-        'code', ligt_in_ggpgebied.code,
-        'naam', ligt_in_ggpgebied.naam
+        'identificatie', ggp_0.identificatie,
+        'code', ggp_0.code,
+        'naam', ggp_0.naam
       ) AS _ref_ligt_in_ggpgebied_gbd_ggp,
       pnd.geometrie
     FROM
       bag_panden AS pnd
     -- SELECT ligt_in_bouwblok
-    LEFT JOIN
-      gebieden_bouwblokken AS ligt_in_bouwblok
-    ON
-      pnd.ligt_in_bouwblok->>'id' = ligt_in_bouwblok.identificatie AND
-      (pnd.ligt_in_bouwblok->>'volgnummer')::int = ligt_in_bouwblok.volgnummer
+    LEFT JOIN mv_bag_pnd_gbd_bbk_ligt_in_bouwblok rel_0
+        ON rel_0.src_id = pnd._id AND rel_0.src_volgnummer = pnd.volgnummer
+    LEFT JOIN gebieden_bouwblokken bbk_0
+        ON rel_0.dst_id = bbk_0._id AND rel_0.dst_volgnummer = bbk_0.volgnummer AND COALESCE(bbk_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     -- SELECT ligt_in_buurt
-    LEFT JOIN
-      gebieden_buurten AS ligt_in_buurt
-    ON
-      ligt_in_bouwblok.ligt_in_buurt->>'id' = ligt_in_buurt.identificatie AND
-      (ligt_in_bouwblok.ligt_in_buurt->>'volgnummer')::int = ligt_in_buurt.volgnummer
+    LEFT JOIN mv_gbd_bbk_gbd_brt_ligt_in_buurt rel_1
+        ON rel_1.src_id = bbk_0._id AND rel_1.src_volgnummer = bbk_0.volgnummer
+    LEFT JOIN gebieden_buurten brt_0
+        ON rel_1.dst_id = brt_0._id AND rel_1.dst_volgnummer = brt_0.volgnummer AND COALESCE(brt_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     -- SELECT ligt_in_wijk
-    LEFT JOIN
-      gebieden_wijken AS ligt_in_wijk
-    ON
-      ligt_in_buurt.ligt_in_wijk->>'id' = ligt_in_wijk.identificatie AND
-      (ligt_in_buurt.ligt_in_wijk->>'volgnummer')::int = ligt_in_wijk.volgnummer
+    LEFT JOIN mv_gbd_brt_gbd_wijk_ligt_in_wijk rel_2
+        ON rel_2.src_id = brt_0._id AND rel_2.src_volgnummer = brt_0.volgnummer
+    LEFT JOIN gebieden_wijken wijk_0
+        ON rel_2.dst_id = wijk_0._id AND rel_2.dst_volgnummer = wijk_0.volgnummer AND COALESCE(brt_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     -- SELECT ligt_in_stadsdeel
-    LEFT JOIN
-      gebieden_stadsdelen AS ligt_in_stadsdeel
-    ON
-      ligt_in_wijk.ligt_in_stadsdeel->>'id' = ligt_in_stadsdeel.identificatie AND
-      (ligt_in_wijk.ligt_in_stadsdeel->>'volgnummer')::int = ligt_in_stadsdeel.volgnummer
+    LEFT JOIN mv_gbd_wijk_gbd_sdl_ligt_in_stadsdeel rel_3
+        ON rel_3.src_id = wijk_0._id AND rel_3.src_volgnummer = wijk_0.volgnummer
+    LEFT JOIN gebieden_stadsdelen sdl_0
+        ON rel_3.dst_id = sdl_0._id AND rel_3.dst_volgnummer = sdl_0.volgnummer AND COALESCE(sdl_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     -- SELECT _ligt_in_ggwgebied
-    LEFT JOIN
-      gebieden_ggwgebieden AS ligt_in_ggwgebied
-    ON
-      ligt_in_buurt._ligt_in_ggwgebied->>'id' = ligt_in_ggwgebied.identificatie AND
-      (ligt_in_buurt._ligt_in_ggwgebied->>'volgnummer')::int = ligt_in_ggwgebied.volgnummer
+    LEFT JOIN mv_gbd_brt_gbd_ggw__ligt_in_ggwgebied rel_4
+        ON rel_4.src_id = brt_0._id AND rel_4.src_volgnummer = brt_0.volgnummer
+    LEFT JOIN gebieden_ggwgebieden ggw_0
+        ON rel_4.dst_id = ggw_0._id AND rel_4.dst_volgnummer = ggw_0.volgnummer AND COALESCE(ggw_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     -- SELECT _ligt_in_ggpgebied
-    LEFT JOIN
-      gebieden_ggpgebieden AS ligt_in_ggpgebied
-    ON
-      ligt_in_buurt._ligt_in_ggpgebied->>'id' = ligt_in_ggpgebied.identificatie AND
-      (ligt_in_buurt._ligt_in_ggpgebied->>'volgnummer')::int = ligt_in_ggpgebied.volgnummer
+    LEFT JOIN mv_gbd_brt_gbd_ggp__ligt_in_ggpgebied rel_5
+        ON rel_5.src_id = brt_0._id AND rel_5.src_volgnummer = brt_0.volgnummer
+    LEFT JOIN gebieden_ggpgebieden ggp_0
+        ON rel_5.dst_id = ggp_0._id AND rel_5.dst_volgnummer = ggp_0.volgnummer AND COALESCE(ggp_0._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
     WHERE
-      (pnd._expiration_date > current_date OR pnd._expiration_date IS NULL)
+      COALESCE(pnd._expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
       AND pnd._date_deleted IS NULL
--- gobcore/views/bag/panden/enhanced_history.sql
+++ gobcore/views/bag/panden/enhanced_history.sql
@@ -1,93 +1,81 @@
-    SELECT
-      pnd.identificatie,
-      pnd.volgnummer,
-      pnd.registratiedatum,
-      pnd.aanduiding_in_onderzoek,
-      pnd.geconstateerd,
-      pnd.oorspronkelijk_bouwjaar,
-      pnd.status,
-      pnd.begin_geldigheid,
-      pnd.eind_geldigheid,
-      pnd.documentnummer,
-      pnd.documentdatum,
-      pnd.naam,
-      pnd.ligging,
-      pnd.aantal_bouwlagen,
-      pnd.hoogste_bouwlaag,
-      pnd.laagste_bouwlaag,
-      pnd.type_woonobject,
-      json_build_object(
-        'identificatie', ligt_in_bouwblok.identificatie,
-        'volgnummer', ligt_in_bouwblok.volgnummer,
-        'code', ligt_in_bouwblok.code
-      ) AS _ref_ligt_in_bouwblok_gbd_bbk,
-      json_build_object(
-        'identificatie', ligt_in_buurt.identificatie,
-        'volgnummer', ligt_in_buurt.volgnummer,
-        'code', ligt_in_buurt.code,
-        'naam', ligt_in_buurt.naam
-      ) AS _ref_ligt_in_buurt_gbd_brt,
-      json_build_object(
-        'identificatie', ligt_in_wijk.identificatie,
-        'volgnummer', ligt_in_wijk.volgnummer,
-        'code', ligt_in_wijk.code,
-        'naam', ligt_in_wijk.naam
-      ) AS _ref_ligt_in_wijk_gbd_wijk,
-      json_build_object(
-        'identificatie', ligt_in_ggwgebied.identificatie,
-        'volgnummer', ligt_in_ggwgebied.volgnummer,
-        'code', ligt_in_ggwgebied.code,
-        'naam', ligt_in_ggwgebied.naam
-      ) AS _ref_ligt_in_ggwgebied_gbd_ggw,
-      json_build_object(
-        'identificatie', ligt_in_stadsdeel.identificatie,
-        'volgnummer', ligt_in_stadsdeel.volgnummer,
-        'code', ligt_in_stadsdeel.code,
-        'naam', ligt_in_stadsdeel.naam
-      ) AS _ref_ligt_in_stadsdeel_gbd_sdl,
-      json_build_object(
-        'identificatie', ligt_in_ggpgebied.identificatie,
-        'volgnummer', ligt_in_ggpgebied.volgnummer,
-        'code', ligt_in_ggpgebied.code,
-        'naam', ligt_in_ggpgebied.naam
-      ) AS _ref_ligt_in_ggpgebied_gbd_ggp,
-      pnd.geometrie
-    FROM
-      bag_panden AS pnd
-    -- SELECT ligt_in_bouwblok
-    LEFT JOIN
-      gebieden_bouwblokken AS ligt_in_bouwblok
-    ON
-      pnd.ligt_in_bouwblok->>'id' = ligt_in_bouwblok.identificatie AND
-      (pnd.ligt_in_bouwblok->>'volgnummer')::int = ligt_in_bouwblok.volgnummer
-    -- SELECT ligt_in_buurt
-    LEFT JOIN
-      gebieden_buurten AS ligt_in_buurt
-    ON
-      ligt_in_bouwblok.ligt_in_buurt->>'id' = ligt_in_buurt.identificatie AND
-      (ligt_in_bouwblok.ligt_in_buurt->>'volgnummer')::int = ligt_in_buurt.volgnummer
-    -- SELECT ligt_in_wijk
-    LEFT JOIN
-      gebieden_wijken AS ligt_in_wijk
-    ON
-      ligt_in_buurt.ligt_in_wijk->>'id' = ligt_in_wijk.identificatie AND
-      (ligt_in_buurt.ligt_in_wijk->>'volgnummer')::int = ligt_in_wijk.volgnummer
-    -- SELECT ligt_in_stadsdeel
-    LEFT JOIN
-      gebieden_stadsdelen AS ligt_in_stadsdeel
-    ON
-      ligt_in_wijk.ligt_in_stadsdeel->>'id' = ligt_in_stadsdeel.identificatie AND
-      (ligt_in_wijk.ligt_in_stadsdeel->>'volgnummer')::int = ligt_in_stadsdeel.volgnummer
-    -- SELECT _ligt_in_ggwgebied
-    LEFT JOIN
-      gebieden_ggwgebieden AS ligt_in_ggwgebied
-    ON
-      ligt_in_buurt._ligt_in_ggwgebied->>'id' = ligt_in_ggwgebied.identificatie AND
-      (ligt_in_buurt._ligt_in_ggwgebied->>'volgnummer')::int = ligt_in_ggwgebied.volgnummer
-    -- SELECT _ligt_in_ggpgebied
-    LEFT JOIN
-      gebieden_ggpgebieden AS ligt_in_ggpgebied
-    ON
-      ligt_in_buurt._ligt_in_ggpgebied->>'id' = ligt_in_ggpgebied.identificatie AND
-      (ligt_in_buurt._ligt_in_ggpgebied->>'volgnummer')::int = ligt_in_ggpgebied.volgnummer
-    WHERE pnd._date_deleted IS NULL
+SELECT
+  pnd.identificatie,
+  pnd.volgnummer,
+  pnd.aanduiding_in_onderzoek,
+  pnd.geconstateerd,
+  pnd.oorspronkelijk_bouwjaar,
+  pnd.status,
+  pnd.begin_geldigheid,
+  pnd.eind_geldigheid,
+  pnd.documentnummer,
+  pnd.documentdatum,
+  pnd.naam,
+  pnd.ligging,
+  pnd.aantal_bouwlagen,
+  pnd.hoogste_bouwlaag,
+  pnd.laagste_bouwlaag,
+  pnd.type_woonobject,
+  json_build_object(
+    'identificatie', bbk_0.identificatie,
+    'code', bbk_0.code
+  ) AS _ref_ligt_in_bouwblok_gbd_bbk,
+  json_build_object(
+    'identificatie', brt_0.identificatie,
+    'code', brt_0.code,
+    'naam', brt_0.naam
+  ) AS _ref_ligt_in_buurt_gbd_brt,
+  json_build_object(
+    'identificatie', wijk_0.identificatie,
+    'code', wijk_0.code,
+    'naam', wijk_0.naam
+  ) AS _ref_ligt_in_wijk_gbd_wijk,
+  json_build_object(
+    'identificatie', ggw_0.identificatie,
+    'code', ggw_0.code,
+    'naam', ggw_0.naam
+  ) AS _ref_ligt_in_ggwgebied_gbd_ggw,
+  json_build_object(
+    'identificatie', sdl_0.identificatie,
+    'code', sdl_0.code,
+    'naam', sdl_0.naam
+  ) AS _ref_ligt_in_stadsdeel_gbd_sdl,
+  json_build_object(
+    'identificatie', ggp_0.identificatie,
+    'code', ggp_0.code,
+    'naam', ggp_0.naam
+  ) AS _ref_ligt_in_ggpgebied_gbd_ggp,
+  pnd.geometrie
+FROM
+  bag_panden AS pnd
+-- SELECT ligt_in_bouwblok
+LEFT JOIN mv_bag_pnd_gbd_bbk_ligt_in_bouwblok rel_0
+    ON rel_0.src_id = pnd._id AND rel_0.src_volgnummer = pnd.volgnummer
+LEFT JOIN gebieden_bouwblokken bbk_0
+    ON rel_0.dst_id = bbk_0._id AND rel_0.dst_volgnummer = bbk_0.volgnummer
+-- SELECT ligt_in_buurt
+LEFT JOIN mv_gbd_bbk_gbd_brt_ligt_in_buurt rel_1
+    ON rel_1.src_id = bbk_0._id AND rel_1.src_volgnummer = bbk_0.volgnummer
+LEFT JOIN gebieden_buurten brt_0
+    ON rel_1.dst_id = brt_0._id AND rel_1.dst_volgnummer = brt_0.volgnummer
+-- SELECT ligt_in_wijk
+LEFT JOIN mv_gbd_brt_gbd_wijk_ligt_in_wijk rel_2
+    ON rel_2.src_id = brt_0._id AND rel_2.src_volgnummer = brt_0.volgnummer
+LEFT JOIN gebieden_wijken wijk_0
+    ON rel_2.dst_id = wijk_0._id AND rel_2.dst_volgnummer = wijk_0.volgnummer
+-- SELECT ligt_in_stadsdeel
+LEFT JOIN mv_gbd_wijk_gbd_sdl_ligt_in_stadsdeel rel_3
+    ON rel_3.src_id = wijk_0._id AND rel_3.src_volgnummer = wijk_0.volgnummer
+LEFT JOIN gebieden_stadsdelen sdl_0
+    ON rel_3.dst_id = sdl_0._id AND rel_3.dst_volgnummer = sdl_0.volgnummer
+-- SELECT _ligt_in_ggwgebied
+LEFT JOIN mv_gbd_brt_gbd_ggw__ligt_in_ggwgebied rel_4
+    ON rel_4.src_id = brt_0._id AND rel_4.src_volgnummer = brt_0.volgnummer
+LEFT JOIN gebieden_ggwgebieden ggw_0
+    ON rel_4.dst_id = ggw_0._id AND rel_4.dst_volgnummer = ggw_0.volgnummer
+-- SELECT _ligt_in_ggpgebied
+LEFT JOIN mv_gbd_brt_gbd_ggp__ligt_in_ggpgebied rel_5
+    ON rel_5.src_id = brt_0._id AND rel_5.src_volgnummer = brt_0.volgnummer
+LEFT JOIN gebieden_ggpgebieden ggp_0
+    ON rel_5.dst_id = ggp_0._id AND rel_5.dst_volgnummer = ggp_0.volgnummer
+WHERE
+    pnd._date_deleted IS NULL

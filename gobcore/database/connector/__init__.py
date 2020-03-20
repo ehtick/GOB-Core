f9f29285908c49e5f4d188003a96e11516113c23
Jaap Koetsier <jaap@chariot-it.nl>
2020-03-20 16:17:08 +0100

    Remove file connector functionality -> moved to gobimport

--- gobcore/database/connector/__init__.py
+++ gobcore/database/connector/__init__.py
@@ -1,9 +1,8 @@
 from gobcore.database.connector.database import connect_to_database
 from gobcore.database.connector.oracle import connect_to_oracle
 from gobcore.database.connector.objectstore import connect_to_objectstore
-from gobcore.database.connector.file import connect_to_file
 from gobcore.database.connector.postgresql import connect_to_postgresql
 from gobcore.database.connector.wfs import connect_to_wfs
 
-__all__ = ['connect_to_database', 'connect_to_objectstore', 'connect_to_file', 'connect_to_oracle',
+__all__ = ['connect_to_database', 'connect_to_objectstore', 'connect_to_oracle',
            'connect_to_postgresql', 'connect_to_wfs']
--- gobcore/database/reader/__init__.py
+++ gobcore/database/reader/__init__.py
@@ -1,9 +1,8 @@
 from gobcore.database.reader.database import query_database, read_from_database           # noqa: F401
 from gobcore.database.reader.oracle import query_oracle, read_from_oracle                 # noqa: F401
 from gobcore.database.reader.objectstore import query_objectstore, read_from_objectstore  # noqa: F401
-from gobcore.database.reader.file import query_file, read_from_file                       # noqa: F401
 from gobcore.database.reader.postgresql import query_postgresql                           # noqa: F401
 from gobcore.database.reader.wfs import query_wfs, read_from_wfs                          # noqa: F401
 
-__all__ = ['read_from_database', 'read_from_objectstore', 'read_from_file', 'read_from_oracle', 'read_from_wfs'] + \
-          ['query_database', 'query_objectstore', 'query_file', 'query_oracle', 'query_postgresql', 'query_wfs']
+__all__ = ['read_from_database', 'read_from_objectstore', 'read_from_oracle', 'read_from_wfs'] + \
+          ['query_database', 'query_objectstore', 'query_oracle', 'query_postgresql', 'query_wfs']

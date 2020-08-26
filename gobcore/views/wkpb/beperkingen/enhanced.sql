2d012fc3ea3e1978e8461d5a0bd8cb8c9ca8721a
Jaap Koetsier <jaap@chariot-it.nl>
2020-08-26 14:45:29 +0200

    PGB-2055 Update WKPB beperkingen enhanced view

--- gobcore/views/wkpb/beperkingen/enhanced.sql
+++ gobcore/views/wkpb/beperkingen/enhanced.sql
@@ -9,8 +9,8 @@ SELECT identificatie
      , eind_geldigheid
 FROM 
     wkpb_beperkingen bpg 
-WHERE "status" -> 'code' = '3' 
-      AND aard -> 'code' <> '3'
+WHERE
+      aard -> 'code' <> '3'
       AND COALESCE(_expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
       AND _date_deleted IS NULL
 ORDER BY 

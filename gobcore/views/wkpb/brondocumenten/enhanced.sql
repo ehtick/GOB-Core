9aafc53d2aba7b825b9b4c063c82e18c01c911cb
Rob Kromwijk <r.kromwijk@amsterdam.nl>
2020-10-14 11:56:27 +0200

    brondocumenten waarvan de einddatum beperking in de toekomst ligt, worden nu ook meegenomen

--- gobcore/views/wkpb/brondocumenten/enhanced.sql
+++ gobcore/views/wkpb/brondocumenten/enhanced.sql
@@ -6,8 +6,7 @@ SELECT bpg.documentnummer,
     bpg.aard
 FROM  
     wkpb_beperkingen bpg
-WHERE bpg.status -> 'code' = '3'
-      AND bpg.aard -> 'code' <> '3'
+WHERE bpg.aard -> 'code' <> '3'
       AND COALESCE(_expiration_date, '9999-12-31'::timestamp without time zone) > NOW()
       AND _date_deleted IS NULL
 ORDER BY 

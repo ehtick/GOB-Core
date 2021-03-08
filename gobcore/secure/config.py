3356953047241d1604d6bb9ca1ed1a82bfad6089
Jaap Koetsier <jaap@chariot-it.nl>
2021-03-08 15:20:51 -0400

    Add HR admin role

--- gobcore/secure/config.py
+++ gobcore/secure/config.py
@@ -7,6 +7,7 @@ User authorization levels and roles
 GOB_ADMIN = "gob_adm"
 GOB_ADMIN_R = "gob_adm_r"
 GOB_DUMP = "gob_dump"
+GOB_HR_ADMIN = "gob_hr_adm"
 GOB_SECURE_ATTRS = "gob_secure_attrs"
 BRK_DATA_TOTAAL = "brk_data_totaal"
 BRK_DATA_BEPERKT = "brk_data_beperkt"

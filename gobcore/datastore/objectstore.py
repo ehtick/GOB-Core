231c64d1faca61ae71eb8982204b6136922ff853
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-01-26 15:05:42 +0100

    fix objectstore read_xls 'NA' as None

--- gobcore/datastore/objectstore.py
+++ gobcore/datastore/objectstore.py
@@ -207,7 +207,7 @@ class ObjectDatastore(Datastore, ListEnabledDatastore, PutEnabledDatastore, Dele
         :return: The list of non-empty rows
         """
         io_obj = io.BytesIO(obj)
-        excel = pandas.read_excel(io=io_obj)
+        excel = pandas.read_excel(io=io_obj, keep_default_na=False, dtype=str)
 
         return self._yield_rows(excel.iterrows(), file_info, config)
 

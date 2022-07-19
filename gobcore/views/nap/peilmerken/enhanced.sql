53b8ed3bac15c3a12a7de9da92797169fe85018e
Jaap Koetsier <jaap@chariot-it.nl>
2022-07-19 10:56:42 +0200

    Change nap_peilmerken enhanced view to run on legacy schema

--- gobcore/views/nap/peilmerken/enhanced.sql
+++ gobcore/views/nap/peilmerken/enhanced.sql
@@ -1,12 +1,12 @@
     SELECT identificatie
     ,      hoogte_tov_nap
     ,      jaar
-    ,      json_build_object('code', merk_code, 'omschrijving', merk_omschrijving) as merk
+    ,      merk
     ,      omschrijving
     ,      windrichting
     ,      x_coordinaat_muurvlak
     ,      y_coordinaat_muurvlak
     ,      rws_nummer
     ,      geometrie
-    FROM  nap_peilmerken
+    FROM  legacy.nap_peilmerken
     WHERE publiceerbaar = TRUE

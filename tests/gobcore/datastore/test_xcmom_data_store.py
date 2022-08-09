9ad984241da128670a4044681de794688aa80850
Roel Kramer <contact@roelkramer.nl>
2022-08-09 11:28:20 +0200

    Make tests not use old XComData object

--- tests/gobcore/datastore/test_xcmom_data_store.py
+++ tests/gobcore/datastore/test_xcmom_data_store.py
@@ -1,7 +1,6 @@
 import json
 
 import pytest
-from pydantic import ValidationError
 
 from gobcore.datastore.xcom_data_store import XComDataStore
 
@@ -19,16 +18,8 @@ class TestXComDataStore:
 
         assert data["contents_ref"] == "/path/to/data.json"
 
-    def test_write_invalid_data_raises_validationerror(self, tmp_file):
-        with pytest.raises(ValidationError):
-            XComDataStore(xcom_path=tmp_file).write({"faulty": "data"})
-
     def test_read_from_arguments(self, tmp_file):
         xcom_data = XComDataStore(xcom_path=tmp_file).parse(
             json.dumps({"contents_ref": "/path/to/data.json"}))
 
-        assert xcom_data.contents_ref == "/path/to/data.json"
-
-    def test_read_from_arguments_raises_validationerror(self, tmp_file):
-        with pytest.raises(ValidationError):
-            XComDataStore(xcom_path=tmp_file).parse(json.dumps({"faulty": "data"}))
+        assert xcom_data["contents_ref"] == "/path/to/data.json"
